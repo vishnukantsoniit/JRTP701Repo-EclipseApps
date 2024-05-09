111352908232a873339dd928abbdd187464cf03d
vishnukantsoniit <vishnukantsoniit@gmail.com>
2024-05-09 19:26:56 +0530

    Initial commit

---
+++ MavenProj03-Flipkart/src/main/java/com/nt/service/FlipkartOperation.java
@@ -0,0 +1,14 @@
+package com.nt.service;
+
+import com.nt.util.StringUtil;
+
+/**
+ * Hello world!
+ *
+ */
+public class FlipkartOperation {
+	public static void main(String[] args) {
+		int tokensCount = StringUtil.countWords("Hello, how are you ? ");
+		System.out.println("words count ::" + tokensCount);
+	}
+}
