a99f4a87cf6cfdb65f5cae56e9c80c9f29a3a4c9
vishnukantsoniit <vishnukantsoniit@gmail.com>
2024-05-10 11:37:57 +0530

    11:37 tl CREATER BRANCHES and added UPI_Payment

---
+++ MavenProj03-Flipkart/src/main/java/com/nt/payment/UPI_Payment.java
@@ -0,0 +1,8 @@
+package com.nt.payment;
+
+public class UPI_Payment {
+	public String doPayment(long phoneno, double amount) {
+		System.out.println("payment is done");
+		return "payment is done";
+	}
+}
